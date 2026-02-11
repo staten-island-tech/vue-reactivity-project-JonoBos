5f3e688ab88076706904b4e600d816607af10975
JonoBos <jonothanb@nycstudents.net>
2026-02-11 11:52:11 -0500

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,12 +5,17 @@
 </template>
 
 <script setup>
+//4-8 length
 const words =[ 
     { Word: 'Peach', Length: '5' },
     { Word: 'Cookie', Length: '6' },
     { Word: 'Orange', Length: '6' },
     { Word: 'Whalen', Length: '5' },
     { Word: 'Computer', Length: '8' },
+    { Word: 'Math', Length: '4' },
+    { Word: 'Code', Length: '4' },
+    { Word: 'Home', Length: '4' },
+    { Word: '', Length: '8' },
 ]
 
 </script>
