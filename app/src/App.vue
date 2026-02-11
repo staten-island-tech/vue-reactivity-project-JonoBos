ebf2fb8ba599c6662108a1aeeb60eb98d8b89be5
JonoBos <jonothanb@nycstudents.net>
2026-02-11 12:31:47 -0500

    array done

--- app/src/App.vue
+++ app/src/App.vue
@@ -7,15 +7,22 @@
 <script setup>
 //4-8 length
 const words =[ 
-    { Word: 'Peach', Length: '5' },
-    { Word: 'Cookie', Length: '6' },
-    { Word: 'Orange', Length: '6' },
-    { Word: 'Whalen', Length: '5' },
-    { Word: 'Computer', Length: '8' },
-    { Word: 'Math', Length: '4' },
-    { Word: 'Code', Length: '4' },
-    { Word: 'Home', Length: '4' },
-    { Word: '', Length: '8' },
+    { Word: 'math', Length: '4' },
+    { Word: 'code', Length: '4' },
+    { Word: 'loop', Length: '4' },
+    { Word: 'style', Length: '5' },
+    { Word: 'cache', Length: '5' },
+    { Word: 'logic', Length: '5' },
+    { Word: 'analog', Length: '6' },
+    { Word: 'delete', Length: '6' },
+    { Word: 'whalen', Length: '6' },
+    { Word: 'hardware', Length: '7' },
+    { Word: 'offline', Length: '7' },
+    { Word: 'network', Length: '7' },
+    { Word: 'computer', Length: '8' },
+    { Word: 'debugger', Length: '8' },
+    { Word: 'download', Length: '8' },
+
 ]
 
 </script>
