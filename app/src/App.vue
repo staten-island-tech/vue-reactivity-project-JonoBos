fefbebea9c815a7f31b83c8410e542059fe45717
JonoBos <jonothanb@nycstudents.net>
2026-02-12 12:31:43 -0500

    buttons created, display started, function created (plan out)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
+    <h1>Coding Hangman</h1>
+    <h2>Choose the Number of letters</h2>
+  <div id="display">{{ divContent }}</div>
+
+    <button id = "choice4" >4 letters</button>
+    <button id = "choice5" >5 letters</button>
+    <button id = "choice6" >6 letters</button>
+    <button id = "choice7" >7 letters</button>
+    <button id = "choice8">8 letters</button>
+
+    <button id ="clear" @click ="clearDiv">RESTART</button>
 
   </div>
 </template>
 
 <script setup>
-//4-8 length
+
+
+
+
 const words =[ 
     { Word: 'math', Length: '4' },
     { Word: 'code', Length: '4' },
@@ -24,6 +38,32 @@ const words =[
     { Word: 'download', Length: '8' },
 
 ]
+
+
+
+function display(words){
+
+
+
+
+//end
+const clearDiv = () => {
+  divContent.value = ''; 
+};
+
+}
+//hangman
+//make 5  buttons with functions to display either 4,5,6,7,8 length words
+//make a random index to choose one of the words
+// correlate the button to the length of choice
+//push the random word into another array
+//create user input
+//ask user to type letter
+//if letter is found in the word show where that letter is in the array (display th letter)
+// else if the letter is found but repeated, display , sorry you've chosen this letter already
+// else( if it isn't found) save that letter in a spearte array (display)
+// display the number of tries the user has used
+//if the number of correct letter is = to world length , display the word and say congrats!
 
 </script>
 
